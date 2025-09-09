5cd3082addac7420d3b028f1c777d08e965cddff
IT2E_GIAN KYLE R. AMBA <ambagiankyle36@gmail.com>
2025-09-09 12:30:29 +0800

    Create Exer2_CarsTester.java

---
+++ Exer2_Amba/Exer2_CarsTester.java
@@ -0,0 +1,12 @@
+public class CarsTester {
+    public static void main(String[] args) {
+        
+        Cars c1 = new Cars("Red", "01", "NO RECORD", "V12", "FERRARI", "NO MODEL");
+        Cars c2 = new Cars();
+
+        c1.displayInfo();
+        c2.displayInfo();
+        
+        
+    }
+}
